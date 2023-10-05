2b181053056389db1c5a4429acdadc6b84670d21
Adam <adamelmadani0129@gmail.com>
2023-10-05 23:54:21 +0000

    2.task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,17 +26,20 @@ def do_deploy(archive_path):
     """deploy archive file"""
     if not os.path.exists(archive_path):
         return(False)
-    put(archive_path, '/tmp/')
-    filename = archive_path.split("/")[1].split(".tgz")[0]
-    cmd1 = 'tar -xzf /tmp/{}.tgz -C /data/web_static/releases\
-            /{}/'.format(filename, filename)
-    run('mkdir -p /data/web_static/releases/{}/'.format(filename))
-    run(cmd1)
-    run('rm /tmp/{}.tgz'.format(filename))
-    run('mv /data/web_static/releases/{}/web_static/* /data/\
-            web_static/releases/{}/'.format(filename, filename))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(filename))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static\
-            /current'.format(filename))
-    return(True)
+    try:
+        put(archive_path, '/tmp/')
+        filename = archive_path.split("/")[1].split(".tgz")[0]
+        cmd1 = 'tar -xzf /tmp/{}.tgz -C /data/web_static/releases\
+                /{}/'.format(filename, filename)
+        run('mkdir -p /data/web_static/releases/{}/'.format(filename))
+        run(cmd1)
+        run('rm /tmp/{}.tgz'.format(filename))
+        run('mv /data/web_static/releases/{}/web_static/* /data/\
+                web_static/releases/{}/'.format(filename, filename))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(filename))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{}/ /data/web_static\
+                /current'.format(filename))
+        return(True)
+    except Exception:
+        return(False)
