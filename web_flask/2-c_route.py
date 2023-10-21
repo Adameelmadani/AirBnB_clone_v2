3eb4b035a265e69b53ed21c9adaac7cb9d510e71
Adam <adamelmadani0129@gmail.com>
2023-10-21 12:12:09 +0000

    2.task

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This is our module
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """
+    This is the root page
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    This is the hbnb page
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """
+    Display c text
+    """
+    text2 = "C " + text.replace("_", " ")
+    return text2
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
