9da270b99b4b0b81fadef62965c8e8a50bfe64a7
Adam <adamelmadani0129@gmail.com>
2023-10-21 00:16:35 +0000

    1.task

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This is our module
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """
+    This is the root page
+    """
+    return 'Hello HBNB!'
+@app.route('/hbnb')
+def hbnb():
+    """
+    This is the hbnb page
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
