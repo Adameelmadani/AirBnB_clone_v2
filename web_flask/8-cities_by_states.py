e1be2b4e2af752631299e190afb145dce2510bc4
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2023-10-23 00:36:25 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,7 +6,6 @@ from flask import Flask
 from flask import render_template
 from models import storage
 from models.state import State
-from os import getenv
 
 app = Flask(__name__)
 
@@ -19,8 +18,7 @@ def states_list():
     Returns html page of states list
     """
     states = storage.all('State').values()
-    s = getenv("HBNB_TYPE_STORAGE")
-    return render_template('8-cities_by_states.html', states=states, storage_t=s)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
